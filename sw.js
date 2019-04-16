3e1af8f33fc52b1ec19baf411d79868b22905c1c
Karol Bulwin <karolbulwin@gmail.com>
2019-04-16 14:43:42 +0200

    sw

--- sw.js
+++ sw.js
@@ -1,74 +1,67 @@
-//importScripts('cache-polyfill.js');
-
-var CACHE_NAME = 'bj-cache';
-var urlsToCache = [
+const CACHE_NAME = 'bj-cache-v2';
+const urlsToCache = [
   '/',
   'index.html',
-  'styles/main.css',
-  'scripts/script.js',
-  'images/icons/blackjack-icon192x192.png'
+  'favicon.ico',
+  'public/stylesheets/style.css',
+  'public/javascripts/main.js',
+  'public/javascripts/for-sw.js',
+  'public/javascripts/detect-mouse.js',
+  'https://fonts.googleapis.com/css?family=Roboto+Slab',
+  'https://use.fontawesome.com/releases/v5.8.1/css/all.css'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', (event) => {
   // Perform install steps
-  'use strict';
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function (cache) {
+      .then((cache) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('activate', function (event) {
-  'use strict';
-  var cacheWhitelist = ['bj-cache'];
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['bj-cache-v2'];
 
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
   );
 });
 
-self.addEventListener('fetch', function (event) {
-  'use strict';
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(function (response) {
+      .then((response) => {
         // Cache hit - return response
         if (response) {
           return response;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function (response) {
+        return fetch(event.request).then(
+          (res) => {
             // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
+            if (!res || res.status !== 200 || res.type !== 'basic') {
+              return res;
             }
 
             // IMPORTANT: Clone the response. A response is a stream
             // and because we want the browser to consume the response
             // as well as the cache consuming the response, we need
             // to clone it so we have two streams.
-            var responseToCache = response.clone();
+            const responseToCache = res.clone();
 
             caches.open(CACHE_NAME)
-              .then(function (cache) {
+              .then((cache) => {
                 cache.put(event.request, responseToCache);
               });
 
